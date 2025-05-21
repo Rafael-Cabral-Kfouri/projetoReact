2c4987de2d6f8e4b6312eec396a941dba250299c
Rafael Cabral <rafaelkfouri@gmail.com>
2025-05-21 16:42:56 -0300

    criei o botão delete

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -7,26 +7,26 @@ function App() {
   const [tasks, setTasks] = useState([
   { 
     id: 1,
-    titulo: 'estudar programação',
+    title: 'estudar programação',
     description: 'se tornar um desenvolvedor fullstack',
     isCompleted: false
   },
   {
     id: 2,
-    titulo: 'estudar inglês',
+    title: 'estudar inglês',
     description: 'se tornar fluente',
     isCompleted: false
   },
   {
     id: 3,
-    titulo: 'estudar matemática',
+    title: 'estudar matemática',
     description: 'se tornar um nerd',
     isCompleted: false
   },
 ]);
 
 function clicouNaTask(taskId){
-  const newTasks = tasks.map(task => {
+  const newTasks = tasks.map((task) => {
   //preciso atualizar essa tarefa
     if (task.id === taskId) {
       return {...task, isCompleted: !task.isCompleted}
@@ -36,15 +36,29 @@ function clicouNaTask(taskId){
   })
   setTasks(newTasks);
 }
+  function deleteNaTask(taskId){
+  const newTasks = tasks.filter(task => task.id != taskId)
+  setTasks(newTasks);
+}
+
+function addListaDeBaixo (title, description) {
+  const newTask = {
+    id: tasks.length + 1,
+    title: title, // pode fazer um 'short hand sintaxe'. Nome da propriedade é igual nome parametro
+    description: description, 
+    isCompleted: false 
+  };
+  setTasks([...tasks, newTask])// equivalente ao tasks.push(newTask) mas não funcioa com push
+}
   
-  return (
+return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4" >
           <h1 className="text-3xl text-slate-100 font-bold text-center">
             Gerenciador de Tarefas</h1>  
 {/* Quando vc quer passar um valor pro componente de baixo vc usa uma props */} 
-      <AddTask />
-      <Tasks tasks={tasks} clicouNaTask={clicouNaTask} />
+      <AddTask addListaDeBaixo={addListaDeBaixo} />
+      <Tasks tasks={tasks} clicouNaTask={clicouNaTask} deleteNaTask={deleteNaTask} />
       </div>
     </div>
   );
--- React/src/components/Tasks.jsx
+++ React/src/components/Tasks.jsx
@@ -1,16 +1,19 @@
-import { ChevronRightIcon } from "lucide-react";
-//agora esse Tasks tem acesso dentro desse props a clicouNaTask e por isso quando eu clicar no botão quero chamar a função clicouNaTask
-function Tasks(props) {
+import { ChevronRightIcon, TrashIcon} from "lucide-react";
+// props é o meio que o arquivo App passa a função 'clicouNaTask' para o arquivo Tasks. Não precisa exportar o arquivo e importar. Então task.id(a tarefa clicada) é passado pelo onClick para TaskId que é o parâmetro de 'clicouNaTask', que passa via props esse valor para a função 'Tasks'. Tasks 
+function Tasks({tasks, clicouNaTask, deleteNaTask}) {
     return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-        {props.tasks.map((task) => (
+        {tasks.map((task) => (
             <li key={task.id} className="flex gap-2">
 {/* quando vc tem uma props que é uma função e vc precisa chamar quando clicar n botão e também passar parâmetro vc passa primeiro a arrowfunction                 */}
-            <button onClick={()=>props.clicouNaTask(task.id)} className={`bg-slate-400 text-left text-white p-2 rounded-md w-full ${task.isCompleted && 'line-through'}`}>
+            <button onClick={() => clicouNaTask(task.id)} className={`bg-slate-400 text-left text-white p-2 rounded-md w-full ${task.isCompleted && 'line-through'}`}>
                 {task.titulo}
                  </button>
             <button className="bg-slate-400 p-2 rounded-md text-white">
                 <ChevronRightIcon />
+            </button>
+            <button onClick={() => deleteNaTask(task.id)} className="bg-slate-400 p-2 rounded-md text-white">
+                <TrashIcon />
             </button>
             </li>
         ))}
